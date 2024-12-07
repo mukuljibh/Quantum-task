002bfc07992f37d7cdd0a78637f95d4161f108f5
Mukul <mukulbhardwaj966@gmail.com>
2024-12-07 12:53:42 +0530

    remove comments for login controller

--- server/api/controller/loginController.js
+++ server/api/controller/loginController.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-undef */
-import jwt from 'jsonwebtoken'
 import { User } from '../model/model.js'
 import bcrypt from 'bcrypt'
 import { createAccessToken } from '../middleware/verifyToken.js'
@@ -13,22 +12,15 @@ export async function loginController(req, res) {
 
         if (!match) return res.status(401).json({ message: "Invalid credentials" })
 
-        // in this login route only iam  generating  access and refresh
         const accessToken = createAccessToken(email)
-        // for testing refresh token expiry 30 s and access token expiration 20s
-        // in this route token are getting stored inside the DB for now i am storing them without hashing 
 
-        //we can change cookies option as per our requirement
         const options = {
             maxAge: 5000000,
             sameSite: 'none',
             secure: true,
             httpOnly: true,
         }
-        // attaching cookies to the res object
         res.cookie("sessionid", accessToken, options)
-        // res.cookie("refreshToken", refreshToken, options)
-
         res.status(200).json({ message: "Login successfull" })
 
 
