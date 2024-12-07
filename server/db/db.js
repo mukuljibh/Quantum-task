d4e6be9009f929fe757d04747da74afdd7904b29
Mukul <mukulbhardwaj966@gmail.com>
2024-12-07 11:21:12 +0530

    added port in  env

--- server/db/db.js
+++ server/db/db.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
 
-const port = process.env.CONNECTION_STRING || 8000
-
 export default function makeDbConnection(app) {
     mongoose
         .connect(process.env.CONNECTION_STRING)
         .then(() => {
-            app.listen(port, () => {
-                console.log(`server Started runing on port ${port}`)
+            app.listen(process.env.PORT, () => {
+                console.log(`server Started runing on port ${process.env.PORT}`)
             })
         })
         .catch((err) => {
