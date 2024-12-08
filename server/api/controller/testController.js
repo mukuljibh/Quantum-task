c6ad4c3d045d80632b44289eca64b585b1b7ba4b
mukuljibh <157892785+mukuljibh@users.noreply.github.com>
2024-12-08 10:24:01 +0000

    Update testController.js

--- server/api/controller/testController.js
+++ server/api/controller/testController.js
@@ -1,4 +1,4 @@
 
 export async function testController(req, res) {
-    res.status(200).json({ message: "cookies recieved" })
+    res.status(200).json({ message: "cookies recieved", sessionid:req.cookies })
 }
